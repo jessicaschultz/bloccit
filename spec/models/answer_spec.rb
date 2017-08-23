9b92259c1dffc230ce140bab4d2f5f34533671f2
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-08-23 14:02:47 -0700

    db rollback to correct question_id in question_spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   let(:question) {Question.new(title: "New Question Title", body: "New Question Body", resolved: false)}
-  let(:answer) {Answer.new(question: question, body: "New Answer Body")}
+  let(:answer) { Answer.new(question: question, body: "New Answer Body")}
 
   it "responds to body" do
     expect(answer).to respond_to(:body)
