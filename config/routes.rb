04d2a011af38528922eecce4beb5fc8231ea716e
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-10-03 13:35:53 -0700

    checkpoint25 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :posts, expect: [:index]
   end
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
