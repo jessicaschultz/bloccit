b03e8b9d7f90172715640b9be1d204e9e5ae67d8
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-10-23 15:54:39 -0700

    checkpoint28 completed and passing all test

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, expect: [:index]
   end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+###couldn't tell if this should be deleted in checkpoint28
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+###end####
+
   # get 'post', to: 'post#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,7 @@ users = User.all
  # Create Comments
  100.times do
    Comment.create!(
+     user: users.sample,
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
